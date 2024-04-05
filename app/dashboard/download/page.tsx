7a44b70fda119eba7cabea08dae2c27ffddb5c9a
Manav Kaushal <manavkaushal756@gmail.com>
2024-04-05 09:09:01 +0530

    download sdk complete

--- app/dashboard/download/page.tsx
+++ app/dashboard/download/page.tsx
@@ -1,151 +1,123 @@
+import DownloadSDKToolbar from "@/components/dashboard/DownloadSDKToolbar";
 import FadeUp from "@/components/global/FadeUp";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { sdkReleases } from "@/lib/mocks/dashboard";
+import Link from "next/link";
 
 type Props = {};
 
 const DownloadPage = (props: Props) => {
   return (
     <FadeUp>
-      <div className="grid gap-16 sm:grid-cols-2">
-        {/* Download card */}
-        {/* <motion.div
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true }}
-          transition={{ duration: 0.5, delay: 0.2 }}
-          variants={{
-            visible: { opacity: 1, y: 0 },
-            hidden: { opacity: 0, y: 40 },
-          }}
-          onClick={() => {
-            if (user?.role === "admin" || user?.role === "owner") {
-              downloadSdk();
-            } else {
-              setShowModal(true);
-            }
-          }}
-          className="relative p-12 space-y-8 duration-200 cursor-pointer bg-glass rounded-xl h-fit group text-neutral-400 hover:text-neutral-200"
-        >
-          <ArchiveBoxArrowDownIcon className="w-[60px] h-[60px]" />
-          <div className="relative">
-            <p className="mt-6 text-2xl font-semibold tracking-tight text-left sm:text-3xl md:text-2xl">
-              Download SDK <small>(v1.0.9)</small>
-              <ArrowRightIcon className="inline-block w-6 h-6 ml-2 font-bold duration-300 ease-linear group-hover:ml-4" />
-            </p>
-            {percentage > 0 && percentage < 100 && (
-              <motion.div
-                className="w-full mt-4 text-left"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                transition={{ duration: 0.5 }}
-              >
-                <Line
-                  className="mt-2"
-                  percent={percentage}
-                  strokeWidth={1}
-                  strokeLinecap={"round"}
-                />
-                <p className="mt-2 text-sm">
-                  {getLabel(percentage)}... {percentage}%
-                </p>
-              </motion.div>
-            )}
-          </div>
-        </motion.div> */}
-        {/* Documentation */}
-        {/* <Link
-          href={config.documentationUrl}
-          className="duration-200 cursor-pointer group text-neutral-400 hover:text-neutral-200"
-          target="_blank"
-        >
-          <motion.div
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ once: true }}
-            transition={{ duration: 0.5, delay: 0.2 }}
-            variants={{
-              visible: { opacity: 1, y: 0 },
-              hidden: { opacity: 0, y: 40 },
-            }}
-            className="relative p-12 space-y-8 bg-glass rounded-xl"
-          >
-            <DocumentTextIcon className="w-[60px] h-[60px]" />
-
-            <p className="flex items-center mt-6 text-2xl font-semibold tracking-tight text-left sm:text-3xl md:text-2xl">
-              Documentation
-              <ArrowRightIcon className="inline-block w-6 h-6 ml-2 font-bold duration-300 ease-linear group-hover:ml-4" />
-            </p>
-          </motion.div>
-        </Link> */}
-      </div>
-
       <Card className="bg-[#161616]">
         <CardHeader>
-          <CardTitle>Downloads</CardTitle>
+          <CardTitle>Getting started</CardTitle>
         </CardHeader>
-        <CardContent>
+        <CardContent className="text-muted-foreground">
           <div className="space-y-3">
             <p>
-              If you&apos;re an Android developer, you should get the latest SDK
-              Platform-Tools from Android Studio&apos;s SDK Manager or from the
-              sdk manager command-line tool. This ensures the tools are saved to
-              the right place with the rest of your Android SDK tools and easily
-              updated.
-            </p>
-            <p>
-              But if you want just these command-line tools, use the following
-              links:
+              You can get the MotionMix SDK with the help of following links.
+              Before you install this SDK:
             </p>
             <ul className="pl-6 space-y-2 list-disc">
-              <li className="cursor-pointer hover:text-foreground text-muted-foreground hover:underline hover:underline-offset-4">
-                Download SDK Platform-Tools for Windows
+              <li>
+                Review all{" "}
+                <span className="text-foreground">system requirements</span>
               </li>
-              <li className="cursor-pointer hover:text-foreground text-muted-foreground hover:underline hover:underline-offset-4">
-                Download SDK Platform-Tools for Mac
-              </li>
-              <li className="cursor-pointer hover:text-foreground text-muted-foreground hover:underline hover:underline-offset-4">
-                Download SDK Platform-Tools for Linux
+              <li>Exit Visual Studio prior to installation</li>
+              <li>
+                Review the{" "}
+                <Link
+                  href="#release-notes"
+                  className="text-foreground hover:underline hover:underline-offset-4"
+                >
+                  Release notes and Known Issues
+                </Link>
               </li>
             </ul>
-            <p>
-              Although these links do not change, they always point to the most
-              recent version of the tools.
-            </p>
+            <DownloadSDKToolbar />
+            <p className="text-sm">Last updated: February 2024</p>
           </div>
         </CardContent>
       </Card>
-
-      {/* Releases information */}
-      <Card className="bg-[#161616] mt-4">
+      <Card className="mt-5 bg-[#161616]" id="system-requirements">
         <CardHeader>
-          <CardTitle>Revisions</CardTitle>
+          <CardTitle>System requirements</CardTitle>
         </CardHeader>
-        <CardContent>
+        <CardContent className="text-muted-foreground">
           <div className="space-y-3">
-            <ul className="space-y-10">
-              {sdkReleases.map((release) => (
-                <li key={release.version} className="first:mt-3">
-                  <p className="text-lg font-semibold text-neutral-200">
-                    {release.version} ({release.date})
-                  </p>
-                  <ul className="pl-6 space-y-4 list-disc">
-                    {release.changes.map((change) => (
-                      <li key={change.subTitle} className="first:mt-2">
-                        <p className="font-semibold">{change.subTitle}</p>
-                        <ul className="pl-10 list-disc">
-                          {change.desc.map((d, i) => (
-                            <li key={i + Math.random()}>{d}</li>
-                          ))}
-                        </ul>
-                      </li>
-                    ))}
-                  </ul>
-                </li>
-              ))}
+            <p>The SDK has the following minimum system requirements:</p>
+            <p className="text-lg text-foreground">
+              Supported operating systems
+            </p>
+            <ul className="pl-6 space-y-2 list-disc">
+              <li>
+                Windows 10 version 1507 or higher: Home, Professional,
+                Education, and Enterprise (LTSB and S are not supported for UWP)
+              </li>
+              <li>
+                Windows Server 2022, Windows Server 2019, Windows Server 2016,
+                and Windows Server 2012 R2 (Command line only)
+              </li>
+              <li>Windows 8.1</li>
+              <li>Windows 7 SP1</li>
+            </ul>
+            <p>(Not all tools are supported on earlier operating systems)</p>
+            <p className="text-lg text-foreground">Hardware requirements</p>
+            <ul className="pl-6 space-y-2 list-disc">
+              <li>1.6 GHz or faster processor</li>
+              <li>1 GB of RAM</li>
+              <li>4 GB of available hard disk space</li>
             </ul>
           </div>
+        </CardContent>
+      </Card>
+      <Card className="mt-5 bg-[#161616]" id="release-notes">
+        <CardHeader>
+          <CardTitle>Release notes and known issues</CardTitle>
+        </CardHeader>
+        <CardContent className="text-muted-foreground">
+          <Accordion type="multiple" className="w-full">
+            {[
+              {
+                id: "q-1",
+                question:
+                  "Windows 11, Build 10.0.22621.3235 (released 2/29/2024)",
+                answer: "Servicing update 10.0.22621.3235.",
+              },
+              {
+                id: "q-2",
+                question:
+                  "Windows 11, Build 10.0.22621.2428 (released 10/24/2023)",
+                answer: "Servicing update 10.0.22621.2428.",
+              },
+              {
+                id: "q-3",
+                question: "Windows 11, Version 22H2, Build 10.0.22621.1778",
+                answer:
+                  "<p>Update 10.0.22621.1778. Highlighted features include:</p><ul><li>WindowTabManager APIs allows applications with tabbed interfaces to provide information on open tabs to the Windows shell.</li><li>Updates to HumanPresence APIs to improve ease-of-use and add new settings for sensors that support human presence capabilities.</li><li>RemoteDesktop APIs allows applications to switch between a remote and local desktop.</li></ul>",
+              },
+            ].map((item) => (
+              <AccordionItem
+                key={item.id}
+                value={item.id}
+                className="accordion-item"
+              >
+                <AccordionTrigger>{item.question}</AccordionTrigger>
+                <AccordionContent>
+                  <div
+                    id="release-note-answers"
+                    dangerouslySetInnerHTML={{ __html: item.answer }}
+                  />
+                </AccordionContent>
+              </AccordionItem>
+            ))}
+          </Accordion>
         </CardContent>
       </Card>
     </FadeUp>
---
+++ components/dashboard/DownloadSDKToolbar.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { api } from "@/services/api";
+import { useState } from "react";
+import { toast } from "sonner";
+import { Button } from "../ui/button";
+
+type Props = {};
+
+const sdkVersions = ["1.2.1", "1.0.9", "1.0.0"];
+
+const DownloadSDKToolbar = (props: Props) => {
+  const [percentage, setPercentage] = useState(0);
+  const [version, setVersion] = useState(sdkVersions[0]);
+  const fileName = `MotionMix_v${version}.unitypackage`;
+
+  const downloadSdk = async () => {
+    const preparingDownloadToast = toast.loading(`Preparing your download...`, {
+      description: `version: v${version}`,
+    });
+    try {
+      const res = await api.apisauce.get("/download/sdk", undefined, {
+        timeout: 25000,
+        responseType: "arraybuffer",
+        onDownloadProgress: (progressEvent) => {
+          if (progressEvent.total) {
+            const percentage = Math.round(
+              (progressEvent.loaded * 100) / progressEvent.total
+            );
+            setPercentage(percentage);
+          }
+        },
+      });
+
+      if (!res.ok) {
+        toast.error(
+          "Oops! Something went wrong while trying to get the file. Please try again later."
+        );
+      }
+
+      const blob = new Blob([res.data as any], {
+        type: "application/octet-stream",
+      });
+      const url = window.URL.createObjectURL(blob);
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", fileName);
+      document.body.appendChild(link);
+      link.click();
+      link.remove();
+      toast.dismiss(preparingDownloadToast);
+      toast.success("Your download is complete.");
+    } catch (error) {
+      console.error("Error downloading file:", error);
+      toast.error(
+        "Oops! Something went wrong while trying to get the file. Please try again later."
+      );
+    }
+  };
+
+  return (
+    <div className="flex items-center space-x-2">
+      <Select value={version} onValueChange={(e) => setVersion(e)}>
+        <SelectTrigger className="w-[180px] text-foreground">
+          <SelectValue />
+        </SelectTrigger>
+        <SelectContent>
+          {sdkVersions.map((option, i) => (
+            <SelectItem key={option} value={option}>
+              v{option}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+      <Button variant="secondary" onClick={downloadSdk}>
+        Download
+      </Button>
+    </div>
+  );
+};
+
+export default DownloadSDKToolbar;
